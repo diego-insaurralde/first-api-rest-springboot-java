07c5336a66b79e51509c127c59df484e7e8e2a77
diego-insaurralde <diego_insaurralde@live.com>
2024-07-13 15:28:15 -0300

    first commit

---
+++ apirest/src/test/java/com/dijub/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dijub.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
